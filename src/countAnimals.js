3f2c9fe4f08a6ab41d52e58dae3bf3c0c6bca744
Bruno Minelli <minellibruno@hotmail.com>
2022-01-25 18:38:06 -0300

    Requisito 05 finalizado.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+
+  if (!animal) {
+    return species.reduce((accAnimal, currAnimal) => {
+      const key = currAnimal.name;
+      const value = currAnimal.residents.length;
+      const object = accAnimal;
+      object[key] = value;
+      return object;
+    }, {});
+  }
+  const { residents } = species.find((thisAnimal) => thisAnimal.name === animal.specie);
+  if (Object.keys(animal).length === 1) return residents.length;
+
+  return residents.reduce((acc, curr) => (curr.sex === animal.sex ? acc + 1 : acc), 0);
 }
 
 module.exports = countAnimals;
