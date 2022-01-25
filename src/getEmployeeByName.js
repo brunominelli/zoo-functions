32b4741c6be781bdcd3380ac3b26d0c3fad52400
Bruno Minelli <minellibruno@hotmail.com>
2022-01-25 16:28:29 -0300

    Requisito 03 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  const { employees } = data;
+  return employees.find((employee) => (
+    (employee.firstName === employeeName || employee.lastName === employeeName)));
 }
 
+console.log(getEmployeeByName());
+console.log(getEmployeeByName('Elser'));
 module.exports = getEmployeeByName;
