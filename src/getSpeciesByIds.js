66a2654cd114f9ae4894b4a958d4285398e2419c
Bruno Minelli <minellibruno@hotmail.com>
2022-01-25 15:34:12 -0300

    Requisito 01 finalizado.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-} 
+function getSpeciesByIds(...ids) {
+  if (!ids) return [];
+  return ids.reduce((arrayIds, speciesId) => {
+    arrayIds.push(data.species.find((animalSpecie) => animalSpecie.id === speciesId));
+    return arrayIds;
+  }, []);
+}
 
 module.exports = getSpeciesByIds;
