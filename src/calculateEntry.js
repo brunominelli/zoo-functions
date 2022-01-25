e6f70cafbc361dcd057e7b070c908dc02ea7898b
Bruno Minelli <minellibruno@hotmail.com>
2022-01-25 19:20:25 -0300

    Requisito 06 finalizado.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const object = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) object.child += 1;
+    else if (entrant.age < 50) object.adult += 1;
+    else object.senior += 1;
+  });
+  return object;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !Object.keys(entrants).length) return 0;
+  let entries = 0;
+  const { prices } = data;
+  const totalEntrants = countEntrants(entrants);
+  Object.entries(totalEntrants).forEach((entrant) => {
+    entries += prices[entrant[0]] * entrant[1];
+  });
+  return entries;
 }
 
 module.exports = { calculateEntry, countEntrants };
