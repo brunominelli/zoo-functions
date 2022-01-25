380193142b74ca3b37435359b739e239a3ee4516
Bruno Minelli <minellibruno@hotmail.com>
2022-01-25 16:05:23 -0300

    Requisito 02 concluido.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const getResidents = species.find((element) =>
+    element.name === animal).residents.every((element) =>
+    element.age >= age);
+  return getResidents;
 }
 
 module.exports = getAnimalsOlderThan;
