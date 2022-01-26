c0b1cef3f2eceaed76fd740a7f6590b4f203bc1d
Bruno Minelli <minellibruno@hotmail.com>
2022-01-26 18:47:02 -0300

    Requisito 09 finalizado.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee = data.employees.find((emplooye) => emplooye.id === id);
+  const responsibleFor = employee.responsibleFor[0];
+  const animals = data.species.find((animal) => animal.id === responsibleFor);
+  const oldestAnimal = animals.residents.reduce((acc, curr) => (curr.age > acc.age ? curr : acc));
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
