dcd616efdddaa3ef62085b4503fed59de6f7bb03
Bruno Minelli <minellibruno@hotmail.com>
2022-01-26 17:29:28 -0300

    Requisito 07 finalizado.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const getLocation = () => species.reduce((acc, curr) => {
+  const { name, location } = curr;
+  if (!acc[location]) acc[location] = [];
+  acc[location].push(name);
+  return acc;
+}, {});
+
+const getNames = ({ sex, sorted }) => species.reduce((acc, curr) => {
+  const { residents, name, location } = curr;
+  if (!acc[location]) acc[location] = [];
+
+  const object = {};
+  const filter = residents.filter((resident) => resident.sex === sex);
+  const list = sex ? filter : residents;
+  const names = list.map((item) => item.name);
+
+  object[name] = sorted ? names.sort() : names;
+  acc[location].push(object);
+  // console.log(acc);
+  return acc;
+}, {});
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || !options.includeNames) return getLocation();
+  return getNames(options);
 }
 
 module.exports = getAnimalMap;
